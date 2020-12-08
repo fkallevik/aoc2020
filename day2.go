ab6f6f0e7dbb9dc8003491c72ec42b7c6c26cbf3
Fredrik Kallevik <fredrik@idrift.no>
2020-12-08 23:25:37 +0100

    Finished day 2, part two.

--- day2.go
+++ day2.go
@@ -6,10 +6,19 @@ import (
 	"strings"
 )
 
+func getEndPos(p int, s string) int {
+	if len(s) < p+1 {
+		return len(s)
+	}
+
+	return p + 1
+}
+
 func Day2() {
 	pwData := strings.Split(ReadFile("input/day2"), "\n")
 
 	partOneValidPws := 0
+	partTwoValidPws := 0
 
 	for _, line := range pwData {
 		// Parse password and policy data.
@@ -42,7 +51,19 @@ func Day2() {
 		if count >= min && count <= max {
 			partOneValidPws++
 		}
+
+		// Validate password for part two.
+		pos1 := policyNumber1 - 1
+		pos2 := policyNumber2 - 1
+
+		pos1Char := pw[pos1:getEndPos(pos1, pw)]
+		pos2Char := pw[pos2:getEndPos(pos2, pw)]
+
+		if (pos1Char == pwLetter && pos2Char != pwLetter) || (pos2Char == pwLetter && pos1Char != pwLetter) {
+			partTwoValidPws++
+		}
 	}
 
-	fmt.Printf("Part One Valid PWs: %v", partOneValidPws)
+	fmt.Printf("Part One Valid PWs: %v\n", partOneValidPws)
+	fmt.Printf("Part Two Valid PWs: %v", partTwoValidPws)
 }
