ed5e28f3f982d828c02751e8d31346c25199156a
Fredrik Kallevik <fredrik@idrift.no>
2020-12-09 23:44:58 +0100

    Finished day 4, part two.

--- day4.go
+++ day4.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -18,8 +20,7 @@ type passport struct {
 
 var passports []passport
 
-func Day4() {
-	input := strings.Split(ReadFile("input/day4"), "\n")
+func getRegisteredPassports(input []string) []passport {
 
 	isNewPassport := true
 
@@ -75,19 +76,97 @@ func Day4() {
 		}
 	}
 
-	// Append the last parsed passport.
-	passports = append(passports, p)
+	// Append the last parsed passport and return.
+	return append(passports, p)
+}
 
-	var validPassports []passport
+func ppFieldsAreEmpty(p passport) bool {
+	return p.byr == "" || p.iyr == "" || p.eyr == "" || p.hgt == "" || p.hcl == "" || p.ecl == "" || p.pid == ""
+}
 
-	// Validate passports.
+func invalidYear(s string, min int, max int) bool {
+	i, err := strconv.Atoi(s)
+
+	return err != nil || len(s) != 4 || i < min || i > max
+}
+
+func invalidHeight(s string) bool {
+	var min, max int
+
+	re := regexp.MustCompile(`^([0-9]{2,3})(cm|in)$`)
+
+	if !re.MatchString(s) {
+		return true
+	}
+
+	substrings := re.FindStringSubmatch(s)
+
+	if len(substrings) != 3 {
+		return true
+	}
+
+	val := substrings[1]
+	unit := substrings[2]
+
+	if unit == "cm" {
+		min = 150
+		max = 193
+	}
+
+	if unit == "in" {
+		min = 59
+		max = 76
+	}
+
+	h, err := strconv.Atoi(val)
+
+	return err != nil || h < min || h > max
+}
+
+func invalidHairColor(s string) bool {
+	re := regexp.MustCompile(`#[0-9a-f]{6}$`)
+
+	return !re.MatchString(s)
+}
+
+func invalidEyeColor(s string) bool {
+	re := regexp.MustCompile(`^(amb|blu|brn|gry|grn|hzl|oth)$`)
+
+	return !re.MatchString(s)
+}
+
+func invalidPassportID(s string) bool {
+	re := regexp.MustCompile(`^[0-9]{9}$`)
+
+	return !re.MatchString(s)
+}
+
+func validatePassports(passports []passport, part int) []passport {
+	var validated []passport
+
 	for _, p := range passports {
-		if p.byr == "" || p.iyr == "" || p.eyr == "" || p.hgt == "" || p.hcl == "" || p.ecl == "" || p.pid == "" {
+		if ppFieldsAreEmpty(p) {
 			continue
 		}
 
-		validPassports = append(validPassports, p)
+		if part == 2 && (invalidYear(p.byr, 1920, 2002) || invalidYear(p.iyr, 2010, 2020) || invalidYear(p.eyr, 2020, 2030) || invalidHeight(p.hgt) || invalidHairColor(p.hcl) || invalidEyeColor(p.ecl) || invalidPassportID((p.pid))) {
+			continue
+		}
+
+		validated = append(validated, p)
 	}
 
-	fmt.Printf("\nPart One - Valid Passports: %v\n", len(validPassports))
+	return validated
+}
+
+func Day4() {
+	input := strings.Split(ReadFile("input/day4"), "\n")
+
+	registeredPassports := getRegisteredPassports(input)
+
+	validPassportsPartOne := validatePassports(registeredPassports, 1)
+	validPassportsPartTwo := validatePassports(registeredPassports, 2)
+
+	fmt.Printf("\nPart One - Valid Passports: %v\n", len(validPassportsPartOne))
+	fmt.Printf("\nPart Two - Valid Passports: %v\n", len(validPassportsPartTwo))
 }
